085674c22f9ff8e1bff465774d1a653bc823a780
sasu.wunu@gmail.com <Sulsiperis>
2023-09-23 00:00:01 +0300

    added salute when all answers correct

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,6 +2,7 @@ import React from "react";
 import { decode } from "html-entities";
 import Shuffled from "./Shuffled";
 import { nanoid } from "nanoid";
+import Confetti from 'react-confetti' 
 
 export default function Questions(props) {
 
@@ -113,6 +114,7 @@ export default function Questions(props) {
     })
     return (
         <div className="questions-content">
+            {(correctCount===6) && <Confetti numberOfPieces="234" />}
             <div className="intro-corner-tr">
                 <svg width="126" height="131" viewBox="0 0 126 131" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M63.4095 71.3947C35.1213 40.8508 -2.68211 11.7816 1.17274 -29.6933C5.43941 -75.599 39.854 -115.359 82.4191 -133.133C122.797 -149.994 170.035 -140.256 205.822 -115.149C235.947 -94.0141 236.823 -53.8756 246.141 -18.271C256.17 20.0508 282.521 60.8106 260.501 93.7792C237.538 128.159 188.991 133.432 147.931 128.768C112.318 124.723 87.7505 97.6768 63.4095 71.3947Z" fill="#FFFAD1"/>
